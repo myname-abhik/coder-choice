62f0a754504ee1dc4260754d4747d4300213e9cd
myname-abhik <abhik16chakrabortty@gmail.com>
2024-04-23 03:45:47 +0530

    start

--- backend.js
+++ backend.js
@@ -9,7 +9,7 @@ const PASSWORD = process.env.PASSWORD
 const PORT = process.env.PORT
 
 
-mongoose.connect(`mongodb+srv://abhik16chakrabortty:${PASSWORD}@cluster0.iuakazn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+mongoose.connect(`mongodb+srv://vercel-admin-user-6626e052f8bd661241c01cd1:DN2gDMPUcKmw00ni@cluster0.iuakazn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 .then(()=>{
     console.log('Connected to MongoDB');
   })
